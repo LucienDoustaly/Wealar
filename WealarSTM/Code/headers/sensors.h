351757b6d86a5340957d075c3d7f25c3fc4078c6
Anthony Genson <anthony.genson@isen.yncrea.fr>
2018-05-29 23:56:15 +0200

    Comments on sensors

--- WealarSTM/Code/headers/sensors.h
+++ WealarSTM/Code/headers/sensors.h
@@ -3,15 +3,39 @@
 
 #include "../headers/utils.h"
 
+/**
+  * ######################################################
+  * Initialization of all sensors
+  * ######################################################
+  */
+
 void sensors_init();
+
+/**
+  * ######################################################
+  * Initialization of weather sensors
+  * ######################################################
+  */
 
 void temperature_sensor_init();
 void humidity_sensor_init();
 void luminosity_sensor_init();
 
+/**
+  * ######################################################
+  * Initialization of presence sensor
+  * ######################################################
+  */
+
 void presence_sensor_init();
 void presence_sensor_GPIO_init();
 void presence_sensor_interruption_init();
+
+/**
+  * ######################################################
+  * Format functions for readable weather data
+  * ######################################################
+  */
 
 int8_t format_temperature(uint16_t ADC_temperature);
 uint8_t format_humidity(uint16_t ADC_humidity, uint8_t temperature);
