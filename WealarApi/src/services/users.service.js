189382070b14fe58a5a7d5adad71f712cb0364ed
Anthony Genson <anthony.genson@isen.yncrea.fr>
2018-05-30 03:11:47 +0200

    Comments on Users actions

--- WealarApi/src/services/users.service.js
+++ WealarApi/src/services/users.service.js
@@ -41,18 +41,18 @@ module.exports = {
 				password: "string"
 			},
 			handler(ctx) {
-				return this.generateHash(ctx.params.password)
+				return this.generateHash(ctx.params.password) // Encrypt the password
 					.then( (res) => {
-						var wealar_id = uuidv4();
+						var wealar_id = uuidv4(); // Generates a random string
 
-						return this.DB_Users.insert(ctx, {
+						return this.DB_Users.insert(ctx, { // Insert the new user in the database
 							id: wealar_id,
-							username: wealar_id,
+							username: wealar_id, // First connection: id = username
 							password: res.data,
 							preferences: Default_Preferences
 						});
 					})
-					.then( () => this.requestSuccess("User Account Created", ctx.params.username) )
+					.then( () => this.requestSuccess("User Account Created", ctx.params.username) ) // Format the response
 					.catch( (err) => this.requestError(CodeTypes.UNKOWN_ERROR));
 			}
 		},
@@ -62,9 +62,9 @@ module.exports = {
 
 			},
 			handler(ctx) {
-				return this.verifyIfAdmin(ctx)
-					.then( () => this.DB_Users.find(ctx, { }) )
-					.then( (res) => this.requestSuccess("Search Complete", res.data) )
+				return this.verifyIfAdmin(ctx) // Verify admin priviledge
+					.then( () => this.DB_Users.find(ctx, { }) ) // Then search for all users in the database
+					.then( (res) => this.requestSuccess("Search Complete", res.data) ) // Format the response
 					.catch( (err) => {
 						if (err.name === 'Nothing Found')
 							return this.requestError(CodeTypes.USERS_NOTHING_FOUND);
@@ -79,11 +79,11 @@ module.exports = {
 
 			},
 			handler(ctx) {
-				return this.verifyIfLogged(ctx)
-					.then( () => this.DB_Users.findById(ctx, {
+				return this.verifyIfLogged(ctx) // Verify if the user is logged
+					.then( () => this.DB_Users.findById(ctx, { // Then search his information in the database
 						id: ctx.meta.user.id
 					}))
-					.then( (res) => this.requestSuccess("Search Complete", res.data) )
+					.then( (res) => this.requestSuccess("Search Complete", res.data) ) // Format the response
 					.catch( (err) => {
 						if (err.name === 'Nothing Found')
 							return this.requestError(CodeTypes.USERS_NOTHING_FOUND);
@@ -98,9 +98,9 @@ module.exports = {
 
 			},
 			handler(ctx) {
-				return this.verifyIfAdmin(ctx)
-					.then( () => this.DB_Users.count(ctx, { }) )
-					.then( (res) => this.requestSuccess("Count Complete", res.data) )
+				return this.verifyIfAdmin(ctx) // Verify admin priviledge
+					.then( () => this.DB_Users.count(ctx, { }) ) // Then count the number of users in the database
+					.then( (res) => this.requestSuccess("Count Complete", res.data) ) // Format the response
 					.catch( (err) => {
 						if (err instanceof MoleculerError)
 							return Promise.reject(err);
@@ -116,15 +116,15 @@ module.exports = {
 				phone: "string"
 			},
 			handler(ctx) {
-				return this.verifyIfLogged(ctx)
-					.then( () => this.DB_Users.updateById(ctx, ctx.meta.user.id, {
+				return this.verifyIfLogged(ctx) // Verify if user is logged
+					.then( () => this.DB_Users.updateById(ctx, ctx.meta.user.id, { // Change the information of the user thanks to his id
 						username: ctx.params.username,
 						phone: ctx.params.phone
 					}))
-					.then( (res) => this.requestSuccess("Changes Saved", true) )
+					.then( (res) => this.requestSuccess("Changes Saved", true) ) // Format the response
 					.catch( (err) => {
 						if (err.name === 'Database Error' && Array.isArray(err.data)){
-							if (err.data[0].type === 'unique' && err.data[0].field === 'username')
+							if (err.data[0].type === 'unique' && err.data[0].field === 'username') // Username must be unique
 								return this.requestError(CodeTypes.USERS_USERNAME_CONSTRAINT);
 						}
 
@@ -141,17 +141,17 @@ module.exports = {
 				newPassword: "string"
 			},
 			handler(ctx) {
-				return this.verifyUserPassword(ctx, ctx.params.oldPassword)
-					.then( () => this.generateHash(ctx.params.newPassword) )
-					.then( (res) => this.DB_Users.updateById(ctx, ctx.meta.user.id, {
+				return this.verifyUserPassword(ctx, ctx.params.oldPassword) // First verify the password of the user
+					.then( () => this.generateHash(ctx.params.newPassword) ) // Encrypt the new password
+					.then( (res) => this.DB_Users.updateById(ctx, ctx.meta.user.id, { // Then store new information about the user in the database, thanks to his id
 						username: ctx.params.username,
 						phone: ctx.params.phone,
 						password: res.data
 					}))
-					.then( (res) => this.requestSuccess("Changes Saved", true) )
+					.then( (res) => this.requestSuccess("Changes Saved", true) ) // Format the response
 					.catch( (err) => {
 						if (err.name === 'Database Error' && Array.isArray(err.data)){
-							if (err.data[0].type === 'unique' && err.data[0].field === 'username')
+							if (err.data[0].type === 'unique' && err.data[0].field === 'username') // Username must be unique
 								return this.requestError(CodeTypes.USERS_USERNAME_CONSTRAINT);
 						}
 
@@ -168,14 +168,14 @@ module.exports = {
 				presenceNotification: "boolean"
 			},
 			handler(ctx) {
-				return this.verifyIfLogged(ctx)
+				return this.verifyIfLogged(ctx) // Verify if user is logged
 					.then( () => {
-						if ((ctx.params.securityMode >= 0) && (ctx.params.securityMode < 3))
+						if ((ctx.params.securityMode >= 0) && (ctx.params.securityMode < 3)) // Check if the security mode is valid
 							return this.requestSuccess("Security Mode Valid", true);
 						else
 							return this.requestError(CodeTypes.USERS_MODE_CONSTRAINT);
 					})
-					.then( () => this.DB_Users.updateById(ctx, ctx.meta.user.id, {
+					.then( () => this.DB_Users.updateById(ctx, ctx.meta.user.id, { // Then updates the new preferences thanks to the user's id
 						preferences: {
 							securityMode: ctx.params.securityMode,
 							smsNotification: ctx.params.smsNotification,
@@ -183,7 +183,7 @@ module.exports = {
 							presenceNotification: ctx.params.presenceNotification
 						}
 					}))
-					.then( (res) => this.requestSuccess("Changes Saved", true) )
+					.then( (res) => this.requestSuccess("Changes Saved", true) ) // Format the response
 					.catch( (err) => {
 						if (err instanceof MoleculerError)
 							return Promise.reject(err);
@@ -199,13 +199,13 @@ module.exports = {
 				newPassword: "string"
 			},
 			handler(ctx) {
-				return this.verifyUserPassword(ctx, ctx.params.oldPassword)
-					.then( () => this.generateHash(ctx.params.newPassword) )
-					.then( (res) => this.DB_Users.updateById(ctx, ctx.meta.user.id, {
+				return this.verifyUserPassword(ctx, ctx.params.oldPassword) // Check that the old password is valid
+					.then( () => this.generateHash(ctx.params.newPassword) ) // Encrypt the new password
+					.then( (res) => this.DB_Users.updateById(ctx, ctx.meta.user.id, { // Then update it to the database
 						password: res.data
 					}))
-					//.then( () => ctx.call("auth.closeAllSessions"))
-					.then( () => this.requestSuccess("Changes Saved", true) )
+					//.then( () => ctx.call("auth.closeAllSessions")) // For security, we close all opened sessions of the user
+					.then( () => this.requestSuccess("Changes Saved", true) ) // Format the response
 					.catch( (err) => {
 						if (err instanceof MoleculerError)
 							return Promise.reject(err);
@@ -221,14 +221,14 @@ module.exports = {
 				role: "string"
 			},
 			handler(ctx) {
-				return this.verifyIfAdmin(ctx)
-					.then( () => this.verifyRole(ctx.params.role) )
-					.then( () => this.DB_Users.findById(ctx, {
+				return this.verifyIfAdmin(ctx) // Verify admin priviledge
+					.then( () => this.verifyRole(ctx.params.role) ) // Check that the role exists
+					.then( () => this.DB_Users.findById(ctx, { // Research information about the user who's role is to change
 						id: ctx.meta.user.id,
 						filter: Filters_Users.username
 					}))
 					.then( (res) => {
-						if ((res.data === ctx.params.username) && (ctx.params.role !== "ADMIN"))
+						if ((res.data === ctx.params.username) && (ctx.params.role !== "ADMIN")) // We don't want to remove admin priviledge to the last admin
 							return this.isLastAdmin(ctx)
 								.then( (res) => {
 									if (res.data === false)
@@ -245,15 +245,15 @@ module.exports = {
 						},
 						filter: Filters_Users.role
 					}))
-					.then( (res) => this.DB_Tokens.removeMany(ctx, {
+					.then( (res) => this.DB_Tokens.removeMany(ctx, { // Close all sessions opened by the user
 						userId: res.data.id
 					}))
-					.then( () => this.DB_Users.updateMany(ctx, {
+					.then( () => this.DB_Users.updateMany(ctx, { // Finally change the user role
 						username: ctx.params.username
 					}, {
 						role: ctx.params.role
 					}))
-					.then( () => this.requestSuccess("Changes Saved", true) )
+					.then( () => this.requestSuccess("Changes Saved", true) ) // Format the response
 					.catch( (err) => {
 						if (err instanceof MoleculerError)
 							return Promise.reject(err);
@@ -268,18 +268,18 @@ module.exports = {
 				password: "string"
 			},
 			handler(ctx) {
-				return this.verifyIfLogged(ctx)
-					.then( () => this.isLastAdmin(ctx) )
+				return this.verifyIfLogged(ctx) // Verify if user is logged
+					.then( () => this.isLastAdmin(ctx) ) // We don't want to remove the last admin
 					.then( (res) => {
 						if (res.data === false)
 							return Promise.resolve(true);
 						else
 							return this.requestError(CodeTypes.USERS_FORBIDDEN_REMOVE);
 					})
-					.then( () => this.verifyUserPassword(ctx, ctx.params.password))
-					.then( () => ctx.call("auth.closeAllSessions") )
-					.then( () => this.DB_Users.removeById(ctx, ctx.meta.user.id))
-					.then( () => this.requestSuccess("Delete Complete", true) )
+					.then( () => this.verifyUserPassword(ctx, ctx.params.password)) // Check that the password is valid
+					.then( () => ctx.call("auth.closeAllSessions") ) // Close all the sessions of the user
+					.then( () => this.DB_Users.removeById(ctx, ctx.meta.user.id)) // Remove entirely the user from the database
+					.then( () => this.requestSuccess("Delete Complete", true) ) // Format the response
 					.catch( (err) => {
 						if (err instanceof MoleculerError)
 							return Promise.reject(err);
@@ -294,25 +294,25 @@ module.exports = {
 				username: "string"
 			},
 			handler(ctx) {
-				return this.verifyIfAdmin(ctx)
-					.then( () => this.DB_Users.findOne(ctx, {
+				return this.verifyIfAdmin(ctx) // Checks admin priviledge
+					.then( () => this.DB_Users.findOne(ctx, { // Search information about the user to be banish
 						query: {
 							username: ctx.params.username
 						},
 						filter: Filters_Users.role
 					}))
 					.then( (res) => {
-						if (res.data.role !== "ADMIN")
-							return this.DB_Tokens.removeMany(ctx, {
+						if (res.data.role !== "ADMIN") // Forbid banishement of an admin
+							return this.DB_Tokens.removeMany(ctx, { // Close all user's sessions
 									userId: res.data.id
 								})
-								.then( () => this.DB_Users.removeMany(ctx, {
+								.then( () => this.DB_Users.removeMany(ctx, { // Remove the user from the database
 									username: ctx.params.username
 								}));
 						else
 							return this.requestError(CodeTypes.USERS_FORBIDDEN_REMOVE);
 					})
-					.then( () => this.requestSuccess("Delete Complete", true) )
+					.then( () => this.requestSuccess("Delete Complete", true) ) // Format the response
 					.catch( (err) => {
 						if (err instanceof MoleculerError)
 							return Promise.reject(err);
@@ -329,12 +329,12 @@ module.exports = {
 				password: "string"
 			},
 			handler(ctx) {
-				return this.verifyIfAdmin(ctx)
-					.then( () => this.verifyUserPassword(ctx, ctx.params.oldPassword) )
-					.then( () => this.DB_Tokens.removeAll(ctx) )
-					.then( () => this.DB_Users.removeAll(ctx) )
-					.then( () => ctx.call("users.createAdminIfNotExists"))
-					.then( () => this.requestSuccess("Delete Complete", true) )
+				return this.verifyIfAdmin(ctx) // Verify admin priviledge
+					.then( () => this.verifyUserPassword(ctx, ctx.params.password) ) // Check if the password is correct
+					.then( () => this.DB_Tokens.removeAll(ctx) ) // Remove all tokens from all users
+					.then( () => this.DB_Users.removeAll(ctx) ) // Remove all users
+					.then( () => ctx.call("users.createAdminIfNotExists")) // Create a default admin user
+					.then( () => this.requestSuccess("Delete Complete", true) ) // Format the response
 					.catch( (err) => {
 						if (err instanceof MoleculerError)
 							return Promise.reject(err);
@@ -349,15 +349,15 @@ module.exports = {
 
 			},
 			handler(ctx) {
-				return this.DB_Users.count(ctx, {
+				return this.DB_Users.count(ctx, { // Count the number of admin user in the database
 						role: "ADMIN"
 					})
 					.then( (res) => {
-						if (res.data === 0) {
+						if (res.data === 0) { // if there is no admin, we create one
 							var wealar_id = uuidv4();
 
-							return this.generateHash("admin")
-								.then( (res) => this.DB_Users.insert(ctx, {
+							return this.generateHash("admin") // Encrypt password
+								.then( (res) => this.DB_Users.insert(ctx, { // Insert the admin in the database
 									username: "admin",
 									password: res.data,
 									role: "ADMIN",
@@ -367,7 +367,7 @@ module.exports = {
 						else
 							return Promise.resolve(true);
 					})
-					.then( () => this.requestSuccess("Admin Exists", true) )
+					.then( () => this.requestSuccess("Admin Exists", true) ) // Format the response
 					.catch( (err) => this.requestError(CodeTypes.UNKOWN_ERROR) );
 			}
 		}
